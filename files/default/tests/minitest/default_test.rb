c8b71f741428441c8202fc821d01810da2876e8a
Mark Morlino <mark@gina.alaska.edu>
2013-10-29 08:15:47 -0800

    added some tests

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -5,7 +5,16 @@ describe 'tomcat7::default' do
   include Helpers::Tomcat7
 
   # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  it 'make sure the tomcat user exists' do
+	user("tomcat").must_exist
+  end
+
+  it 'make sure that /etc/init.d/tomcat7 exists' do
+    file('/etc/init.d/tomcat7').must_exist
+  end
+
+  it 'make sure that /opt/tomcat/conf/server.xml exists' do
+    file('/opt/tomcat/conf/server.xml').must_exist
   end
 
 end
